72651c0ece89a41afc3f4e8fcdb06b5fd842d6d5
Daniel Ramirez <dramirezb@ucenfotec.ac.cr>
2017-11-26 17:36:43 -0600

    Se agrego las clases tramiteFacturacion, tramiteVentaProducto, tramiteInventario, tramiteEntregaProducto.

---
+++ src/main/java/com/cenfotec/develectronicos/entities/tramiteConsultaInventario.java
@@ -0,0 +1,34 @@
+package com.cenfotec.develectronicos.entities;
+
+public class tramiteConsultaInventario extends Tramite {
+
+	
+	
+	
+	public tramiteConsultaInventario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public tramiteConsultaInventario(Documento doc) {
+		super(doc);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void encryptMessage() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void decryptMessage() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
