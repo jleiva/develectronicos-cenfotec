5697488a4a6b1b038b28a8fbb74e7ed44cba758a
Jose Leiva <jdleiva@backcountry.com>
2017-11-26 18:07:43 -0600

    Elimino comentarios y alineo espacio en blanco

--- src/main/java/com/cenfotec/develectronicos/entities/Departamento.java
+++ src/main/java/com/cenfotec/develectronicos/entities/Departamento.java
@@ -1,14 +1,5 @@
 package com.cenfotec.develectronicos.entities;
 
-// Crear las clases por Departamento
-// Ventas
-// Bodega
-// Contabilidad
-
-// Recomendacion usar Herencia
-// Analizar si cada Departamento debe tener un atributo de tipo Tramite
-// donde cree el tipo de Tramite especifico que administra
-
 public class Departamento {
 	String idDept = "";
 	String nombreDept = "";
@@ -16,22 +7,17 @@ public class Departamento {
 	public String getIdDept() {
 		return idDept;
 	}
+	
 	public void setIdDept(String idDept) {
 		this.idDept = idDept;
 	}
+	
 	public String getNombreDept() {
 		return nombreDept;
 	}
+	
 	public void setNombreDept(String nombreDept) {
 		this.nombreDept = nombreDept;
-	}
-	
-	// Departamento ventas = new Departamento();
-	// ventas.iniciarTramiteVentaProducto(prodId);
-	
-	// iniciarTramiteVentaProducto(prodId) {
-	// 	Tramite tramite01 = new VentaProducto(prodId, idDept);	
-	//}
-	
+	}	
 	
 }
--- src/main/java/com/cenfotec/develectronicos/entities/Empleado.java
+++ src/main/java/com/cenfotec/develectronicos/entities/Empleado.java
@@ -9,24 +9,31 @@ public class Empleado {
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public String getApellido() {
 		return apellido;
 	}
+	
 	public String getId() {
 		return id;
 	}
+	
 	public String getIdDept() {
 		return idDept;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public void setIdDept(String idDept) {
 		this.idDept = idDept;
 	}
