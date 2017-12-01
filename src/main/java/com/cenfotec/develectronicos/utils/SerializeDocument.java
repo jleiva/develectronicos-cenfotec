a969b9bdf9ceb98bc0b34e0d5cbeb843d8180d53
Jose Leiva <jdleiva@backcountry.com>
2017-11-30 23:36:37 -0600

    Agrego Unit Test para clase SerializeDocument

--- src/main/java/com/cenfotec/develectronicos/utils/SerializeDocument.java
+++ src/main/java/com/cenfotec/develectronicos/utils/SerializeDocument.java
@@ -1,16 +1,16 @@
 package com.cenfotec.develectronicos.utils;
 
 import com.google.gson.Gson;
-import com.cenfotec.develectronicos.entities.Documento;
+import com.cenfotec.develectronicos.entities.Orden;
 
 public class SerializeDocument {
 	private static Gson gson = new Gson();
 	
-	public static String toJSON(Documento documento) {
+	public static String toJSON(Orden documento) {
 		return gson.toJson(documento);
 	}
 	
-	public static Documento fromJSON(String jsonString) {
-		return gson.fromJson(jsonString, Documento.class);
+	public static Orden fromJSON(String jsonString) {
+		return gson.fromJson(jsonString, Orden.class);
 	}
 }
---
+++ src/test/java/com/cenfotec/develectronicos/utils/SerializeDocumentTests.java
@@ -0,0 +1,46 @@
+package com.cenfotec.develectronicos.utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cenfotec.develectronicos.entities.Orden;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerializeDocumentTests {
+	private SerializeDocument util;
+	
+	@Before
+	public void setUp() {
+		util = new SerializeDocument();
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test
+	public void serializarDeDocumentoAjsonRetornaString() {
+		Orden documento = new Orden("idDoc02", "VentaProducto", "Jose Leiva", "MAM0516", "");
+		String json = util.toJSON(documento);
+		
+		assertTrue(json instanceof String);
+	}
+	
+	@SuppressWarnings("static-access")
+	@Test
+	public void serializarDeStringADocumento() {
+		String jsonString = "{\"idDoc\":\"idDoc02\", \"tipoTramite\":\"VentaProducto\", \"responsable\":\"Jose Leiva\", \"productoId\":\"MAM0516\"}";
+		Orden documento = util.fromJSON(jsonString);
+		
+		assertTrue(documento instanceof Orden);
+		assertEquals("idDoc02", documento.getIdDoc());
+		assertEquals("VentaProducto", documento.getTipoTramite());
+		assertEquals("Jose Leiva", documento.getResponsable());
+		assertEquals("MAM0516", documento.getProductoId());
+	}
+}
