c9bf5baa65544755b7d88b3400028d760e6d91aa
Leizar <leizar@leizar.com>
2017-12-02 13:25:54 -0600

    DeptID to String
    Agregado tambien enum para departamentos

--- src/main/java/com/cenfotec/develectronicos/entities/departments/Contabilidad.java
+++ src/main/java/com/cenfotec/develectronicos/entities/departments/Contabilidad.java
@@ -1,16 +1,21 @@
 package com.cenfotec.develectronicos.entities.departments;
 
 import com.cenfotec.develectronicos.entities.Departamento;
+import com.cenfotec.develectronicos.utils.enums.TipoDepartamento;
 
 public class Contabilidad extends Departamento {
 
-	public Contabilidad(int idDept, String nombreDept) {
+	public Contabilidad(String idDept, String nombreDept) {
 		super(idDept, nombreDept);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Contabilidad() {
+		super(TipoDepartamento.CON.toString(), "CONTABILIDAD");
+	}
 
 	@Override
-	public int getIdDept() {
+	public String getIdDept() {
 		// TODO Auto-generated method stub
 		return super.getIdDept();
 	}
