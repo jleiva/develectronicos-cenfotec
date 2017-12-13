bf4b6eea16be2fa081edfeeab7edeed5056a4af6
Jose Leiva <jdleiva@backcountry.com>
2017-12-12 22:39:10 -0600

    Modifico metodo guardarOrden

--- src/main/java/com/cenfotec/develectronicos/controllers/OrdenController.java
+++ src/main/java/com/cenfotec/develectronicos/controllers/OrdenController.java
@@ -3,12 +3,23 @@ package com.cenfotec.develectronicos.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.cenfotec.develectronicos.data.DocumentoDAO;
 import com.cenfotec.develectronicos.entities.Documento;
 import com.cenfotec.develectronicos.entities.Orden;
+import com.cenfotec.develectronicos.entities.extras.OrdenCompra;
+import com.cenfotec.develectronicos.entities.extras.OrdenEntrega;
+import com.cenfotec.develectronicos.entities.extras.OrdenFacturacion;
+import com.cenfotec.develectronicos.entities.extras.OrdenInventario;
+import com.cenfotec.develectronicos.utils.EncryptManager;
+import com.cenfotec.develectronicos.utils.SerializeDocument;
+import com.cenfotec.develectronicos.utils.enums.DAOType;
 import com.cenfotec.develectronicos.utils.enums.TipoOrden;
+import com.cenfotec.develectronicos.utils.factory.DAOFactory;
 import com.cenfotec.develectronicos.utils.factory.OrdenFactory;
 
 public class OrdenController {
+	DocumentoDAO dao = DAOFactory.crearDAO(DAOType.JSON_FILE);
+	private static EncryptManager encryptManager = new EncryptManager();
 	
 	private ArrayList <Orden> ordenesCompra;
 	private ArrayList <Orden> ordenesInventario;
@@ -63,11 +74,18 @@ public class OrdenController {
 	}
 
 	private void guardarOrden(Documento ord) {
-		// TODO Auto-generated method stub
+		String idDoc = ord.getIdDoc();
+		String idDept = ord.getIdDep();
+		String jsonDoc = SerializeDocument.toJSON(ord);
+		
+		try {
+			byte[] encryptedData = encryptManager.encryptMessage(jsonDoc, idDept+"-");
+			dao.insert(idDoc, encryptedData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
-
-	
 	
 	public Orden seleccionarOrden(List<Orden> Ordenes, String idDoc) {
 		
@@ -101,6 +119,5 @@ public class OrdenController {
 		// TODO Auto-generated method stub
 		
 	}
-
 
 }
